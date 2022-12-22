662af342c27b0ec86df06073a750a4d20cc20133
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-12-22 18:34:51 -0500

    Initial commit

--- be-having.js
+++ be-having.js
@@ -1,9 +1,61 @@
 import { register } from 'be-hive/register.js';
 import { define } from 'be-decorated/DE.js';
 export class BeHaving extends EventTarget {
-    makeBe(pp) {
+    #observer;
+    #queries = new Map();
+    async makeBe(pp) {
+        const { make, self } = pp;
+        const rootNode = self.getRootNode();
+        this.#observer = new MutationObserver(mutations => {
+            mutations.forEach(({ addedNodes }) => {
+                addedNodes.forEach(async (node) => {
+                    if (!(node instanceof Element))
+                        return;
+                    for (const key in make) {
+                        const rule = make[key];
+                        //TODO - memoize
+                        let cssSelector = key;
+                        if (hasCapitalLetterRegExp.test(key)) {
+                            if (!this.#queries.has(key)) {
+                                const { getQuery } = await import('trans-render/lib/specialKeys.js');
+                                this.#queries.set(key, getQuery(key));
+                            }
+                            const qry = this.#queries.get(key);
+                            cssSelector = qry.query;
+                        }
+                        if (node.matches(cssSelector)) {
+                            await this.#processEl(node, rule, make);
+                        }
+                    }
+                });
+            });
+        });
+        this.#observer.observe(rootNode, {
+            childList: true,
+            subtree: true,
+        });
+        for (const key in make) {
+            let cssSelector = key;
+            const rule = make[key];
+            if (hasCapitalLetterRegExp.test(key)) {
+                if (!this.#queries.has(key)) {
+                    const { getQuery } = await import('trans-render/lib/specialKeys.js');
+                    this.#queries.set(key, getQuery(key));
+                }
+                const qry = this.#queries.get(key);
+                cssSelector = qry.query;
+            }
+            rootNode.querySelectorAll(cssSelector).forEach(async (node) => {
+                await this.#processEl(node, key, make);
+            });
+        }
+    }
+    async #processEl(matchingEl, key, make) {
+        const { makeItBe } = await import('trans-render/lib/makeBe.js');
+        makeItBe(matchingEl, key, make);
     }
 }
+const hasCapitalLetterRegExp = /[A-Z]/;
 const tagName = 'be-having';
 const ifWantsToBe = 'having';
 const upgrade = 'script';
--- be-having.ts
+++ be-having.ts
@@ -1,12 +1,67 @@
 import {register} from 'be-hive/register.js';
 import {define, BeDecoratedProps} from 'be-decorated/DE.js';
 import {Actions, PP, PPE, VirtualProps, Proxy, ProxyProps, PPP} from './types';
+import {BeHaving as BeHavingType, QueryInfo, CSSSelectorBeHavingMap} from 'trans-render/lib/types';
 
 export class BeHaving extends EventTarget implements Actions{
-    makeBe(pp: PP){
 
+    #observer: MutationObserver | undefined;
+    #queries: Map<string, QueryInfo> = new Map();
+    async makeBe(pp: PP){
+        const {make, self} = pp;
+        const rootNode = self.getRootNode() as DocumentFragment;
+        this.#observer = new MutationObserver(mutations => {
+            mutations.forEach(({addedNodes}) => {
+                addedNodes.forEach(async node => {
+                    if(!(node instanceof Element)) return;
+                    for(const key in make){
+                        const rule = make[key];
+                        //TODO - memoize
+                        let cssSelector = key;
+                        if(hasCapitalLetterRegExp.test(key)){
+                            if(!this.#queries.has(key)){
+                                const {getQuery} = await import('trans-render/lib/specialKeys.js');
+                                this.#queries.set(key, getQuery(key));
+                            }
+                            const qry = this.#queries.get(key)!;
+                            cssSelector = qry.query;
+                            
+                        }
+                        if(node.matches(cssSelector)){
+                            await this.#processEl(node, rule, make);
+                        }
+                    }
+                })
+            });
+        });
+        this.#observer.observe(rootNode, {
+            childList: true,
+            subtree: true,
+        });
+        for(const key in make){
+            let cssSelector = key;
+            const rule = make[key];
+
+            if(hasCapitalLetterRegExp.test(key)){
+                if(!this.#queries.has(key)){
+                    const {getQuery} = await import('trans-render/lib/specialKeys.js');
+                    this.#queries.set(key, getQuery(key));
+                }
+                const qry = this.#queries.get(key)!;
+                cssSelector = qry.query;
+            }
+            rootNode.querySelectorAll(cssSelector).forEach(async node => {
+                await this.#processEl(node, key, make);
+            })
+        }
+    }
+    async #processEl(matchingEl: Element, key: string,  make: CSSSelectorBeHavingMap){
+        const {makeItBe} = await import('trans-render/lib/makeBe.js');
+        makeItBe(matchingEl, key, make);
     }
 }
+
+const hasCapitalLetterRegExp = /[A-Z]/;
 
 const tagName = 'be-having';
 
